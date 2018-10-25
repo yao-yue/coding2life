2b4fa2dcda57651c9765d6082c183ff57caa134f
longbits <2316570512@qq.com>
2018-10-25 21:49:56 +0800

    一点修改

--- 小程序/buycar-master/pages/index/index.js
+++ 小程序/buycar-master/pages/index/index.js
@@ -4,8 +4,8 @@ var app = getApp()
 Page({
   data: {
     imgUrls: [
-      "http://103.27.4.65/files/banner/20160815/147123112168.jpg",
-        "http://103.27.4.65/files/banner/20160815/14712455440.jpg"
+      "https://haitao.nosdn1.127.net/2fs3CsacfyO1Lko47H11qk3C4IpcT18010242333_1920_506.jpg?imageView&thumbnail=1920x0&quality=90",
+        "https://haitao.nos.netease.com/qyZ0uE5GPfOEldhW2T1809140947_1920_400.jpg?imageView&thumbnail=1920x0&quality=95&type=webp"
     ],
     indicatorDots: true, //是否显示面板指示点
     autoplay: true, //是否自动切换
@@ -45,42 +45,42 @@ Page({
         {
             id:"471",
             title:"喜多多 椰果王礼盒 200ml*10罐",
-            imgurl:"http://103.27.4.65/files/product/20160926/147485679272_small.jpg",
+            imgurl:"#",
             price:"30.00",
             money:"31.80"
         },
         {
             id:"470",
             title:"喜多多 牛奶花生 370g*12罐",
-            imgurl:"http://103.27.4.65/files/product/20160925/147479458751_small.jpg",
+            imgurl:"#",
             price:"32.00",
             money:"36.00"
         },
         {
             id:"469",
             title:"喜多多 桂圆莲子八宝粥 360g*12罐",
-            imgurl:"http://103.27.4.65/files/product/20160925/147479295775_small.jpg",
+            imgurl:"#",
             price:"32.00",
             money:"36.00"
         },
         {
             id:"468",
             title:"喜多多 冰糖雪梨椰果 280g*12罐",
-            imgurl:"http://103.27.4.65/files/product/20160925/147479161349_small.jpg",
+            imgurl:"#",
             price:"38.00",
             money:"42.00"
         },
         {
             id:"467",
             title:"喜多多 什锦椰果罐头 370g 整箱12瓶 果肉果粒",
-            imgurl:"http://103.27.4.65/files/product/20160925/147478997549_small.jpg",
+            imgurl:"#",
             price:"38.00",
             money:"42.00"
         },
         {
             id:"463",
             title:"好丽友派 巧克力清新抹茶味216g",
-            imgurl:"http://103.27.4.65/files/product/20160821/147177248285_small.jpg",
+            imgurl:"#",
             price:"10.00",
             money:"11.00"
         },
@@ -89,42 +89,42 @@ Page({
        {
             id:"467",
             title:"喜多多 什锦椰果罐头 370g 整箱12瓶 果肉果粒",
-            imgurl:"http://103.27.4.65/files/product/20160925/147478997549_small.jpg",
+            imgurl:"#",
             price:"38.00",
             money:"42.00"
         },
         {
             id:"463",
             title:"好丽友派 巧克力清新抹茶味216g",
-            imgurl:"http://103.27.4.65/files/product/20160821/147177248285_small.jpg",
+            imgurl:"#",
             price:"10.00",
             money:"11.00"
         },
         {
             id:"471",
             title:"喜多多 椰果王礼盒 200ml*10罐",
-            imgurl:"http://103.27.4.65/files/product/20160926/147485679272_small.jpg",
+            imgurl:"#",
             price:"30.00",
             money:"31.80"
         },
         {
             id:"470",
             title:"喜多多 牛奶花生 370g*12罐",
-            imgurl:"http://103.27.4.65/files/product/20160925/147479458751_small.jpg",
+            imgurl:"#",
             price:"32.00",
             money:"36.00"
         },
         {
             id:"469",
             title:"喜多多 桂圆莲子八宝粥 360g*12罐",
-            imgurl:"http://103.27.4.65/files/product/20160925/147479295775_small.jpg",
+            imgurl:"#",
             price:"32.00",
             money:"36.00"
         },
         {
             id:"468",
             title:"喜多多 冰糖雪梨椰果 280g*12罐",
-            imgurl:"http://103.27.4.65/files/product/20160925/147479161349_small.jpg",
+            imgurl:"#",
             price:"38.00",
             money:"42.00"
         }  
