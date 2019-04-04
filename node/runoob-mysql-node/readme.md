6e4d005864b941140d57d9fd9f84b1e783ae4e8e
hbw-cloud <2316570512@qq.com>
2019-04-04 16:32:04 +0800

    菜鸟教程node-mysql  体验良好

---
+++ node/runoob-mysql-node/readme.md
@@ -0,0 +1,11 @@
+在mysql中导入数据：
+
+创建数据库：
+use test;
+set names utf8
+source  D：/websites.sql
+
+
+查看一个表 
+desc tables;
+
---
+++ node/runoob-mysql-node/test.js
@@ -0,0 +1,76 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '123456',
+  database : 'test'
+});
+ 
+connection.connect();
+ 
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
+
+
+// var  sql = 'SELECT * FROM websites';
+// //查询表   
+// connection.query(sql,function (err, result) {
+//         if(err){
+//           console.log('[SELECT ERROR] - ',err.message);
+//           return;
+//         }
+ 
+//        console.log('--------------------------SELECT----------------------------');
+//        console.log(result);
+//        console.log('------------------------------------------------------------\n\n');  
+// });
+
+
+//插入数据 
+// var  addSql = 'INSERT INTO websites(Id,name,url,alexa,country) VALUES(0,?,?,?,?)';
+// var  addSqlParams = ['菜鸟工具', 'https://c.runoob.com','23453', 'CN'];
+// //增
+// connection.query(addSql,addSqlParams,function (err, result) {
+//         if(err){
+//          console.log('[INSERT ERROR] - ',err.message);
+//          return;
+//         }        
+ 
+//        console.log('--------------------------INSERT----------------------------');
+//        //console.log('INSERT ID:',result.insertId);        
+//        console.log('INSERT ID:',result);        
+//        console.log('-----------------------------------------------------------------\n\n');  
+// });
+
+
+//更新数据
+// var modSql = 'UPDATE websites SET name = ?,url = ? WHERE Id = ?';
+// var modSqlParams = ['菜鸟移动站-new', 'https://m.runoob.com',6];
+// //改
+// connection.query(modSql,modSqlParams,function (err, result) {
+//    if(err){
+//          console.log('[UPDATE ERROR] - ',err.message);
+//          return;
+//    }        
+//   console.log('--------------------------UPDATE----------------------------');
+//   console.log('UPDATE affectedRows',result.affectedRows);
+//   console.log('-----------------------------------------------------------------\n\n');
+// });
+ 
+//删除数据
+var delSql = 'DELETE FROM websites where id=6';
+//删
+connection.query(delSql,function (err, result) {
+        if(err){
+          console.log('[DELETE ERROR] - ',err.message);
+          return;
+        }        
+ 
+       console.log('--------------------------DELETE----------------------------');
+       console.log('DELETE affectedRows',result.affectedRows);
+       console.log('-----------------------------------------------------------------\n\n');  
+});
+ 
+connection.end();
