be919096c6c67a0ec330a563a6bcccb7b454e196
hbw-cloud <2316570512@qq.com>
2019-04-04 15:14:55 +0800

    菜鸟上的node简单教程 还是挺不错的

---
+++ node/runboob-demo/express_demo3.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+ 
+app.use(express.static('public'));
+ 
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+ 
+var server = app.listen(8081, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+})
+
+
+//在浏览器中输入  127.0.0.1/8081/images/vue_logo.jpg   可以查找到图片资源
+
---
+++ node/runboob-demo/readme.md
@@ -0,0 +1,8 @@
+node.js菜鸟教程的一些demo练习
+
+***body-parser*** 
+  node.js 中间件，用于处理 JSON, Raw, Text 和 URL 编码的数据。
+***cookie-parser*** 
+  这就是一个解析Cookie的工具。通过req.cookies可以取到传过来的cookie，并把它们转成对象。
+***multer*** 
+  node.js 中间件，用于处理 enctype="multipart/form-data"（设置表单的MIME编码）的表单数据
